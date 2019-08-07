add85248b5e4a2ea1a139eab3fcc9f205254b8bd
Kamilimak123 <krychcik16@gmail.com>
2019-08-07 21:59:02 +0200

    first heroku deploy

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(INDEX);
 });
 
 io.on('connection', function (socket) {
@@ -18,6 +22,6 @@ io.on('connection', function (socket) {
     });
 });
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+http.listen(PORT, function () {
+    console.log('listening on *:' + PORT);
 });
