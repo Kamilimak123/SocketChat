e752ac7124eb750b480c9c78800bf93678e39b10
Kamilimak123 <krychcik16@gmail.com>
2019-08-08 12:34:26 +0200

    'user left chat' message

--- index.js
+++ index.js
@@ -10,14 +10,30 @@ app.get('/', function (req, res) {
     res.sendFile(INDEX);
 });
 
+let Users = [];
+
 io.on('connection', function (socket) {
     console.log('a user connected');
+
+    socket.on('new user', function (msg) {
+        let user = {
+            socketid: socket.id,
+            nickname: msg
+        }
+        Users.push(user);
+        io.emit('chat message', "User " + msg + " has joined the chat!");
+    });
 
     socket.on('chat message', function (msg) {
         io.emit('chat message', msg);
     });
 
     socket.on('disconnect', function () {
+        let i = 0;
+        while(Users[i].socketid !== socket.id){
+            i++;
+        }
+        io.emit('chat message', "User " + Users[i].nickname + " has left the chat!");
         console.log('user disconnected');
     });
 });
